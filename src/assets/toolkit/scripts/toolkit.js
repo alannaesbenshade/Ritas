2ec553208d1598df71dc30a4ff96af25035a92f4
alannaesbenshade <alannaesbenshade@cedarville.edu>
2017-12-20 18:31:25 -0500

    Initial commit

---
+++ src/assets/toolkit/scripts/toolkit.js
@@ -0,0 +1,15 @@
+/**
+ * Toolkit JavaScript
+ */
+
+'use strict';
+
+// $(document).ready(function(){
+//   console.log("this is not the code you are looking for...")
+//
+// $(".dropdown-example").on("hover", function(e){
+//
+// });
+//
+//
+// });
